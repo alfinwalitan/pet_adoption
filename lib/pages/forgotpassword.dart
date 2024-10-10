5d097049d035f9c5553dd37069b34ed99ef8f33b
Alfin Tan <alfin6010@gmail.com>
2024-10-10 19:00:05 +0700

    Adding forgotpasword page

--- lib/pages/forgotpassword.dart
+++ lib/pages/forgotpassword.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pet_adoption_app_project/const.dart';
+import 'package:pet_adoption_app_project/pages/signin.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
@@ -8,8 +10,109 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/ScreenAssets/Forgotpassword.jpeg",
+                      width: 205),
+                  const Text(
+                    "Forgot your password?",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: orange),
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    "Enter your email and we will send reset link immediately!",
+                    style: TextStyle(color: subtitle),
+                  ),
+                  const SizedBox(height: 10),
+
+                  // Email
+                  Container(
+                    margin: const EdgeInsets.all(8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.deepPurple.withOpacity(.3),
+                    ),
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Email is Required";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.email),
+                        border: InputBorder.none,
+                        label: Text("Email"),
+                      ),
+                    ),
+                  ),
+
+                  // Forgot Password Button
+                  const SizedBox(height: 10),
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width * .9,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: green,
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Reset Your Password",
+                        style: TextStyle(color: white),
+                      ),
+                    ),
+                  ),
+
+                  // Navigate
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account?",
+                          style: TextStyle(color: subtitle)),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignIn()),
+                          );
+                        },
+                        child: const Text("Login Now!",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
